309a16ebe8fd3cbdb0fc26c355c21323ca75f0ba
Atul Kumar Singh <ace1353058@gmail.com>
2025-08-04 19:09:15 +0530

    third commit

--- factory-app/src/pages/Downloads/Downloads.jsx
+++ factory-app/src/pages/Downloads/Downloads.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import styles from './Downloads.module.css';
 
 const Downloads = () => {
   return (
-    <div>Downloads</div>
-  )
-}
+    <div className={styles.container}>
+      <h2>Downloads</h2>
+      <p>You can view or download our product catalog below:</p>
 
-export default Downloads
+      <div className={styles.pdfViewer}>
+        <iframe
+          src="/catalog.pdf"
+          width="100%"
+          height="600px"
+          title="Product Catalog"
+        ></iframe>
+
+        <a href="/catalog.pdf" download className={styles.downloadBtn}>
+          Download PDF
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Downloads;
